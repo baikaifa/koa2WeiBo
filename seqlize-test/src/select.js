d1af58db6cc083bb6d169bed1073cdb58669e328
baikaifa <1956841815@qq.com>
2020-05-31 20:05:19 +0800

    连接池

--- seqlize-test/src/select.js
+++ seqlize-test/src/select.js
@@ -46,15 +46,58 @@ const { Blog, User } = require('./model')
     // )
 
     //查询总数
-    const blogListAndCount = await Blog.findAndCountAll({
-        limit: 2,//限制本次查询2条
-        offset: 0,//跳过多少条
-        order: [[
-            'id', 'desc'
-        ]]
+    // const blogListAndCount = await Blog.findAndCountAll({
+    //     limit: 2,//限制本次查询2条
+    //     offset: 0,//跳过多少条
+    //     order: [[
+    //         'id', 'desc'
+    //     ]]
+    // })
+    // console.log('blogListAndCount',
+    //     blogListAndCount.count,//所有的总数，不考虑分页
+    //     blogListAndCount.rows.map(blog => blog.dataValues)
+    // )
+
+    //连表查询1
+    // const blogListWithUser = await Blog.findAndCountAll({
+    //     order: [
+    //         ['id', 'desc']
+    //     ],
+    //     include: [
+    //         {
+    //             model: User,
+    //             attributes: ['userName', 'nickName'],
+    //             where: {
+    //                 userName: 'zhansan'
+    //             }
+    //         },
+    //     ]
+    // })
+
+
+    // console.log('blogListWithUser',
+    //     blogListWithUser.count,
+    //     blogListAndCount.rows.map(blog => {
+    //         const blogVal = blog.dataValues
+    //         blogVal.user = blogVal.user.dataValues
+    //         return blogVal
+    //     })
+    // )
+
+    //连表查询2
+    const userListWithBlog = await User.findAndCountAll({
+        attributes: ['userName', 'nickName'],
+        include: [{
+            model: Blog
+        }]
     })
-    console.log('blogListAndCount',
-        blogListAndCount.count,//所有的总数，不考虑分页
-        blogListAndCount.rows.map(blog => blog.dataValues)
-    )
+    console.log('userListWithBlog',
+        userListWithBlog.count,
+        userListWithBlog.rows.map(user => {
+            const userVal = user.dataValues
+            console.log('serVal...', userVal)
+
+            return userVal
+        }))
+
 })()
--- seqlize-test/src/seq.js
+++ seqlize-test/src/seq.js
@@ -4,6 +4,13 @@ const conf = {
     host: 'localhost',
     dialect: 'mysql'
 }
+//线上环境，使用连接池
+
+config.pool = {
+    max: 5,//连接池中最大的连接数量
+    min: 0,//最小
+    idle: 10000//如果一个连接池10s内没有被使用，则释放
+}
 
 const seq = new Sequelize('koa2_weibo_db', 'root', 'Mysql_2018', conf)
 
